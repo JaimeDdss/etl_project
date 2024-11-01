406d4ef209f5ff7e45df99117a3bf096b4f3ea33
Jaime Daniel <jjjsouza2015@gmail.com>
2024-10-31 22:34:20 -0300

    add extract_data function and gitignore

---
+++ extract_data.py
@@ -0,0 +1,38 @@
+#Carga de dados:
+#Carregue o dataset "CEPEA_20241029160016.xls" em memória como dataframes;
+#Extraia os dados do IPCA por meio da API do Banco Central do Brasil (BCB);
+import pandas as pd
+import numpy as np
+import requests
+
+# Função que extrai os dados da API
+def extract_data(file_path):
+    try:
+        response = requests.get(file_path)
+        if response.status_code == 200:
+            print(f"status: {response.status_code}")
+            data = response.json()
+            df = pd.DataFrame(data)
+            return df
+        else:
+            print(f"Erro ao acessar a URL: {response.status_code}")
+            return None
+    except requests.RequestException as e:
+        print(f"Erro na requisição: {e}")
+        return None
+
+# Função para carregar os dados no terminal
+def load_data(df):
+    if df is not None:
+        print(df.head())
+    else:
+        (f"Data frame vazio ou não carregou")
+
+if __name__ == "__main__":
+    # URL da API
+    file_path = "https://api.bcb.gov.br/dados/serie/bcdata.sgs.10844/dados?formato=json"
+    
+    # Extrair dados e carregar no terminal
+    df = extract_data(file_path)
+    load_data(df)
+
