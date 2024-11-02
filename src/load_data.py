abc90c001bcb20c33c20f9554ad8343549079d9c
Jaime Daniel <jjjsouza2015@gmail.com>
2024-11-02 18:09:26 -0300

    update load_data

--- src/load_data.py
+++ src/load_data.py
@@ -1,8 +1,6 @@
 
 # Imports
 import pandas as pd
-import pyarrow.parquet as pq
-import pyarrow as pa
 import os
 from config.config import CSV_FILE_PATH1, XLS_FILE_PATH2
 
@@ -13,18 +11,13 @@ def get_data_from_files():
 
     # Parte que lê o arquivo csv
     df = pd.read_csv(file_paths[0])
-    print('dados csv: ')
-    print(df.head())
-
-    # Parte que lê o arquivo xls
+       # Parte que lê o arquivo xls
     df1 = pd.read_excel(file_paths[1])
-    print('dados xls: ')
-    print(df1.head())
-
+    
     return df, df1
 
 # Teste da função 
 if __name__ == '__main__':
-    get_data_from_files()
+    df_csv, df_xls = get_data_from_files()
 
 
